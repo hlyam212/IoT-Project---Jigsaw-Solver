283b965fffe3aaab5637c207aac74277658de775
Ralph Liang <ycliang@ntu.edu.tw>
2023-01-11 17:36:00 +0800

    UPDATE

--- pages/static/script.js
+++ pages/static/script.js
@@ -2,11 +2,8 @@ function P00Start(){
     window.location.href = window.location.origin+'/PS01ModelSelect';
 }
 function P01Select(picFileName){
-    debugger
     window.location.href = window.location.origin+'/PS02Upload?picFileName='+picFileName;
 }
-function P02Submit(){
-    debugger
-    val=this.location.search.split("=")[1]
-    document.getElementById("P02modalNum").value=val
+function P02Exit(){
+    window.location.href = window.location.origin+'/runExit'
 }
